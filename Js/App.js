3bedd4a03ce0c372a3f76c56b14530f3cc431aa9
irfanhadi93 <atuhn444@gmail.com>
2025-07-06 10:03:05 +0700

    Create App.js

---
+++ Js/App.js
@@ -0,0 +1,8 @@
+import { initForm, tampilkanDaftar, tampilkanTotal } from './ui.js';
+
+// Inisialisasi saat halaman dimuat
+document.addEventListener('DOMContentLoaded', () => {
+  initForm();
+  tampilkanDaftar();
+  tampilkanTotal();
+});
