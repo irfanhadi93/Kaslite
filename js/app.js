74c98b27bb10b4f3b9ed3cc112db893de959d863
irfanhadi93 <atuhn444@gmail.com>
2025-07-10 11:30:47 +0700

    Delete js/app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-import { initForm, tampilkanDaftar, tampilkanTotal } from './ui.js';
-
-// Inisialisasi saat halaman dimuat
-document.addEventListener('DOMContentLoaded', () => {
-  initForm();
-  tampilkanDaftar();
-  tampilkanTotal();
-});
